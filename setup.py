0345aa0077452c67b17b5f803f512b83de51bafd
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2021-11-17 20:22:36 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hmc5883l',
+    version='0.0.1',
+    description='Interface functions for HMC8553L Magnetometer (I2C digital compass)',
+    author='Richard Hull',
+)
